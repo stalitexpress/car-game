2651dc80a91ad40146a5db81f5a1d882fc469e93
funnyware <tinodoan1996@gmail.com>
2023-12-20 16:34:22 -0800

    v1.01 update
    [+] added the ability to enter and exit car (;enter, ;exit).
    [+] added comments.
    [+] added inCar cond.
    [+] added the requirement to be inCar to start and stop.
    [+] added commands to ;help list.
    [+] other minor tweaks.

--- BallSack.py
+++ BallSack.py
@@ -1,54 +1,59 @@
 print("~~~~~~~~~~~~ CAR GAME ~~~~~~~~~~~~")
 print("Type ';help' for help with commands")
-
 # Variables command and started
-
 command = ""
 started = False
-
+inCar = False
 # Main code
-
 while True:
     command = input(";")
-    if command.lower() == "start":  # Start command
-        if started:
-            print("Car is already started! ")  # Start command stop
+    if command.lower() == "enter":  # Enter car command
+        if inCar:
+            print("Already in car! ")
         else:
-            started = True
-            print("Started car... ")  # Start command output
-    elif command.lower() == "stop":  # Stop command
-        if not started:
-            print("Car is already stopped! ")  # Stop command stop
+            inCar = True
+            print("Entering car... ")
+    elif command.lower() == "exit":
+        if inCar:
+            inCar = False
+            print("Exiting car... ")
         else:
-            started = False
-            print(":Stopped car... ")  # Stop command output
+            print("Already out of car! ")
+    
+    if inCar:  # Check if inCar is True before allowing start and stop commands
+        if command.lower() == "start":  # Start command
+            if started:
+                print("Car is already started! ")  # Start command stop
+            else:
+                started = True
+                print("Started car... ")  # Start command output
+        elif command.lower() == "stop":  # Stop command
+            if not started:
+                print("Car is already stopped! ")  # Stop command stop
+            else:
+                started = False
+                print("Stopped car... ")  # Stop command output
+        elif command.lower() == "steer_r":  # Steer right command
+            if not started:
+                print("Car must be started to steer! ")
+            else:
+                print("Steering right! ")
+        elif command.lower() == "steer_l":  # Steer left command
+            if not started:
+                print("Car must be started to steer! ")
+            else:
+                print("Steering left! ")
     elif command.lower() == "help":  # help command
-
         # Help command output
-
-        print("1. ';start' Starts the car; ")
-        print("2. ';stop' Stops the car; ")
-        print("3. ';steer_r' Steers the car right; ")
-        print("4. ';steer_l' Steers the car left; ")
-        print("5. ';quit/;q' Quits the game; ")
-        print("6. ';help' Lists commands; ")
-
-        # Steer function
-
-    elif command.lower() == "steer_r":
-        if not started:
-            print("Car must be started to steer! ")
-        else:
-            print("Steering right! ")
-    elif command.lower() == "steer_l":
-        if not started:
-            print("Car must be started to steer! ")
-        else:
-            print("Steering left! ")
-    elif command.lower() == "q":
-        print("Quitting... ")
-        break
-    elif command.lower() == "quit":
+        print("1. ';start' Starts the car. ")
+        print("2. ';stop' Stops the car. ")
+        print("3. ';steer_r' Steers the car right. ")
+        print("4. ';steer_l' Steers the car left. ")
+        print("5. ';enter' Enters the car. Hint; first command; ")
+        print("6. ';exit' Exits the car; ")
+        print("7. ';quit/;q' Quits the game. ")
+        print("8. ';help' Lists commands. ")
+    elif command.lower() == "q" or command.lower() == "quit":  # Quit command
         print("Quitting... ")
         break
     else:
