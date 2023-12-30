eca7c1ba1894f85632ee810514494f1ef852420f
funnyware <tinodoan1996@gmail.com>
2023-12-30 10:27:20 -0800

    Update BallSack.py
    v1.02 update 
    patch soon

--- BallSack.py
+++ BallSack.py
@@ -9,52 +9,55 @@ while True:
     command = input(";")
     if command.lower() == "enter":  # Enter car command
         if inCar:
-            print("Already in car! ")
-        else:
+            print("You are already in the car.")
+        elif inCar is False:
             inCar = True
-            print("Entering car... ")
+            print("Entering the car... ")
     elif command.lower() == "exit":
         if inCar:
             inCar = False
-            print("Exiting car... ")
+            print("Exiting the car... ")
+        elif inCar is False:
+            print("You are already out of the car.")
+    if command.lower() == "start":  # Start command
+        if started:
+            if inCar:
+                print("The car is already started.")
+        elif inCar:
+            started = True
+            print("Starting the car... ")
         else:
-            print("Already out of car! ")
-    
-    if inCar:  # Check if inCar is True before allowing start and stop commands
-        if command.lower() == "start":  # Start command
+            print("You must be in the car to start it.")
+        if command.lower() == "stop":  # Stop command
+            if not started:
+                print("The car is already stopped.")
             if started:
-                print("Car is already started! ")  # Start command stop
-            else:
-                started = True
-                print("Started car... ")  # Start command output
-        elif command.lower() == "stop":  # Stop command
-            if not started:
-                print("Car is already stopped! ")  # Stop command stop
-            else:
                 started = False
-                print("Stopped car... ")  # Stop command output
-        elif command.lower() == "steer_r":  # Steer right command
-            if not started:
-                print("Car must be started to steer! ")
-            else:
-                print("Steering right! ")
-        elif command.lower() == "steer_l":  # Steer left command
-            if not started:
-                print("Car must be started to steer! ")
-            else:
-                print("Steering left! ")
-    elif command.lower() == "help":  # help command
+                print("Stopping the car... ")
+    elif command.lower() == "help":  # Help command
         # Help command output
-        print("1. ';start' Starts the car. ")
-        print("2. ';stop' Stops the car. ")
-        print("3. ';steer_r' Steers the car right. ")
-        print("4. ';steer_l' Steers the car left. ")
-        print("5. ';enter' Enters the car. Hint; first command; ")
-        print("6. ';exit' Exits the car; ")
-        print("7. ';quit/;q' Quits the game. ")
-        print("8. ';help' Lists commands. ")
-    elif command.lower() == "q" or command.lower() == "quit":  # Quit command
-        print("Quitting... ")
+        print("1. ';start' Starts the car.")
+        print("2. ';stop' Stops the car.")
+        print("3. ';steer_r' Steers the car right.")
+        print("4. ';steer_l' Steers the car left.")
+        print("5. ';enter' Enters the car. (Hint: First command.)")
+        print("6. ';exit' Exits the car.")
+        print("7. ';quit' or ';q' Quits the game.")
+        print("8. ';help' Lists commands.")
+    elif command.lower() == "steer_r":
+        if not started:
+            print("You must start the car to steer.")
+        else:
+            print("Steering right!")
+    elif command.lower() == "steer_l":
+        if not started:
+            print("You must start the car to steer.")
+        else:
+            print("Steering left!")
+    elif command.lower() == "q" or command.lower() == "quit":
+        print("Quitting the game...")
         break
     else:
-        print("Invalid command. Type ';help' for a list of commands")
+        print("Invalid command. Type ;help for help.")
+
+# End of program
